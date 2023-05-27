aedaed23898bc7d2b08de293832fcbbc5e045fa9
simon0820s <simon0820s@gmail.com>
2023-05-27 13:32:29 -0500

    create api

--- easy.py
+++ easy.py
@@ -1,11 +1,9 @@
 import cv2
 import easyocr
 
-def reader ():
+def reader (img):
+    
     reader = easyocr.Reader(["es"], gpu=False)
-    image = cv2.imread("tif.jpeg")
-    result = reader.readtext(image, detail=0)
-    print(result)
-if __name__=='__main__':
-    reader()
-
+    result = reader.readtext(img, detail=0)
+    
+    return result
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask,request,jsonify
 import numpy as np
 from base64 import b64decode
 import re
-from class_p import Prediction
+from easy import reader
+import cv2
 
 
 app=Flask(__name__)
@@ -13,15 +14,17 @@ def upload():
     img_b64=data.get("image")
     img=prepare_img(img_b64)
     
-    return text.to_jsonify()
-
+    result = reader(img)
+    print(result)
+    
+    return result
+    
 def prepare_img(img_b64):
     img = re.sub('^data:image/.+;base64,', '', img_b64)
     img = np.frombuffer(b64decode(img), dtype=np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_UNCHANGED)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = np.array([img]).astype(float)/255
     
-    return img    
+    return img  
+  
 if __name__ == '__main__':
     app.run(debug=True, port=2809, host='0.0.0.0')
