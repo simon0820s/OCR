f9222d90a7f025608328e320c9c1c6191bfc68e6
simon0820s <simon0820s@gmail.com>
2023-07-01 12:16:44 -0500

    create simple quality validator

--- models/quality_validator.py
+++ models/quality_validator.py
@@ -1,22 +1,24 @@
-import numpy as np
-from keras.applications import VGG16
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
 from PIL import Image
+import numpy as np
 
 def run(img):
-    # Cargar el modelo preentrenado VGG16 sin la capa final (clasificación)
-    base_model = VGG16(weights='imagenet', include_top=True)
+    w, h = img.size
+    print(verify_dimentions(w,h))
+    print(verify_size(w,h))
+    
+def verify_dimentions(w,h):
+    expected_width = 400
+    expected_height = 200
 
-    # Realizar la predicción de calidad de imagen
-    pred = base_model.predict(img)
-    print("pred: ",pred)
-
-    # Interpretar la predicción
-    if pred[0] < 0.5:
-        print("La imagen es de baja calidad.")
+    if w >= expected_width and h >= expected_height:
+        return True
     else:
-        print("La imagen es de buena calidad.")
+        return False
+
+def verify_size(w,h):
+    return True if w*h>460000 else False
+
 
 if __name__=='__main__':
-    run()
+    img = Image.open("./IMG_20230326_095637_509.jpg")
+    run(img)
--- prepare_img.py
+++ prepare_img.py
@@ -9,6 +9,5 @@ def run(img_b64):
     img = re.sub("^data:image/.+;base64,", "", img_b64)  # delete prefix
     img = np.frombuffer(b64decode(img), dtype=np.uint8)  # decode b64 tobinary
     img = cv2.imdecode(img, cv2.IMREAD_UNCHANGED)  # decode to cv2
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # to gray scale
 
     return img
